98c329d7c59ee33077e6f12b1c3529743d3c0b44
Arhat161 <Arhat161@users.noreply.github.com>
2022-03-13 19:41:58 +0300

    First commit of "Lambda task 1"

---
+++ src/ru/netology/Calculator.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    //BinaryOperator<Integer> devide = (x, y) -> x / y; // A 'devide by zero' error may occur!
+    BinaryOperator<Integer> devide = (x, y) -> {
+        int res = 0;
+        if(y != 0) res = x / y;
+        return res;
+    };
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+}
